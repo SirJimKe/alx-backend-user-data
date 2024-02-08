5f01a8831ebc3f422f2a0c6fb4ec504c4000b6ff
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-02-08 04:40:06 +0300

    db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@ import re
 from typing import List
 import logging
 from logging import StreamHandler
+import mysql.connector
+import os
 
 
 class RedactingFormatter(logging.Formatter):
@@ -15,25 +17,27 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__()  # Corrected super() call
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.msg, self.SEPARATOR)
+                                  record.msg, self.SEPARATOR)  # Used imported filter_datum
         return super().format(record)
 
 
 PII_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """Obfuscates specified fields in a log message"""
-    for field in fields:
-        message = re.sub(fr'{field}\s*=\s*(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+def get_db() -> mysql.connector.MySQLConnection:
+    """Returns a connector to the MySQL database"""
+    db_connect = mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
+    return db_connect
 
 
 def get_logger() -> logging.Logger:
@@ -48,3 +52,12 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
 
     return logger
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Obfuscates specified fields in a log message"""
+    for field in fields:
+        message = re.sub(fr'{field}\s*=\s*(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
