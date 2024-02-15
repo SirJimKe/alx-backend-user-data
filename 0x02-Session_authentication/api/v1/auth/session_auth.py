dfea76c8075054bcbef082078c5de155f2d1731e
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-02-15 23:12:25 +0300

    fixed identation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,7 +18,7 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-         """Returns a user ID based on a session ID"""
+        """Returns a user ID based on a session ID"""
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
