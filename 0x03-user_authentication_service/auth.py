dad5476715dbafe323fa3247947201366d8f963f
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-02-22 05:29:23 +0300

    get session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """hashed password"""
 import bcrypt
+import uuid
 from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
@@ -32,3 +33,36 @@ class Auth:
             user = self._db.add_user(email=email,
                                      hashed_password=hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if login credentials are valid."""
+        try:
+            user = self._db.find_user_by(email=email)
+            provided_password = password.encode('utf-8')
+
+            if isinstance(user.hashed_password, str):
+                hashed_password = user.hashed_password.encode('utf-8')
+            else:
+                hashed_password = user.hashed_password
+
+            if bcrypt.checkpw(provided_password, hashed_password):
+                return True
+            else:
+                return False
+        except NoResultFound:
+            return False
+
+    @staticmethod
+    def _generate_uuid() -> str:
+        """Generate a new UUID string"""
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str) -> str:
+        """Create a new session for the user and return the session ID."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
